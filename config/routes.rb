abd0a36a291888ebde32b989e218989f9fc9861d
Spencer Alan <spencer.alan@icloud.com>
2017-11-22 15:56:25 -0800

    add routes for plant actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "plant#show", as: "home"
+
+  post "new" => "plant#new", as: :new_plant
+  post "water" => "plant#water", as: :water_plant
+  post "sun" => "plant#sun", as: :sun_plant
+  post "fertilize" => "plant#fertilize", as: :fertilize_plant
 end
